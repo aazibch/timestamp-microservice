75061dcee97a1e59e38eefdbb95af5ab6666d3e5
Aazib Chaudhry <aazibch47@gmail.com>
2021-01-05 17:59:57 +0500

    Log message to console when app starts, saying on which port it is running

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const timestampRoutes = require('./routes/timestamp');
 app.use('/api/timestamp', timestampRoutes);
 
 const port = process.env.PORT || 5000;
-app.listen(port);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
