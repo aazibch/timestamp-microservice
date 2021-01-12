8459e36925b34c2da1e07f4966dba03a7be1400b
Aazib Chaudhry <aazibch47@gmail.com>
2021-01-12 21:48:53 +0500

    Add front-end pages

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const timestampRoutes = require('./routes/timestamp');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/timestamp', timestampRoutes);
 
 const port = process.env.PORT || 5000;
